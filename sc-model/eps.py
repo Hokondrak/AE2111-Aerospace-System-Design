8add1d279923032cabee3cd97aadbc4494d97294
Hokondrak <mmamykin2006@gmail.com>
2026-09-16 09:59:18 +0200

    Constants

--- sc-model/eps.py
+++ sc-model/eps.py
@@ -1,12 +1,12 @@
 import numpy as np
-
+import constants.py as C
 propulsion = 'chemical'
 
 
 ADCSpower = 304 # [W]
 OBDHpower = 1.5 # 0.5-1.5 [W]
 COMMpower = 30 # 20-30 [W]
-PROPpower = 
+PROPpower = 0 # [W]
 ##CASE 1: Chemical Propulsion
 if propulsion == 'checmical':
 
@@ -15,4 +15,4 @@ if propulsion == 'checmical':
 ###CASE 2: Electrical Propulsion
 
 if propulsion == 'electrical':
-
+    PROPpower = 10000 # [W]
